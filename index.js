8bb7ee0a4e5af8bf8f394b08ee63c5f0e7bd07f4
Aiden McClelland <DR-BoneZ@users.noreply.github.com>
2018-11-05 16:14:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,11 @@ function Cryptr(secret) {
             throw new Error('value must not be null or undefined');
         }
 
-        const iv = crypto
-            .randomBytes(16)
-            .toString('hex')
-            .slice(0, 16);
+        const iv = crypto.randomBytes(16);
         const cipher = crypto.createCipheriv(algorithm, key, iv);
         const encrypted = cipher.update(String(value), 'utf8', 'hex') + cipher.final('hex');
 
-        return iv + encrypted;
+        return iv.toString('hex') + encrypted;
     };
 
     this.decrypt = function decrypt(value) {
@@ -32,8 +29,8 @@ function Cryptr(secret) {
         }
 
         const stringValue = String(value);
-        const iv = stringValue.slice(0, 16);
-        const encrypted = stringValue.slice(16);
+        const iv = Buffer.from(stringValue.slice(0, 32), 'hex');
+        const encrypted = stringValue.slice(32);
 
         const decipher = crypto.createDecipheriv(algorithm, key, iv);
         return decipher.update(encrypted, 'hex', 'utf8') + decipher.final('utf8');
